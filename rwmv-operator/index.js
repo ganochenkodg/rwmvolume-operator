a089adf6bd5d29d834722ae54797431077d34a87
Dmitrii Ganochenko <dganochenko.work@gmail.com>
2023-02-20 17:12:22 +0100

    watch custom resources

--- rwmv-operator/index.js
+++ rwmv-operator/index.js
@@ -1,10 +1,45 @@
-const k8s = require('@kubernetes/client-node');
+import * as k8s from '@kubernetes/client-node';
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const watch = new k8s.Watch(kc);
 
-k8sApi.listNamespacedPod('default').then((res) => {
-  console.log(res.body);
+// Then this function determines what flow needs to happen
+// Create, Update or Destroy?
+async function onEvent(phase, apiObj) {
+  log(`Received event in phase ${phase}.`);
+  console.log(apiObj);
+}
+// Helpers to continue watching after an event
+function onDone(err) {
+  log(`Connection closed. ${err}`);
+  watchResource();
+}
+
+async function watchResource() {
+  log('Watching API');
+  return watch.watch(
+    '/apis/dganochenko.work/v1alpha1/rwmvolumes',
+    {},
+    onEvent,
+    onDone
+  );
+}
+// The watch has begun
+async function main() {
+  await watchResource();
+}
+
+// Helper to pretty print logs
+function log(message) {
+  console.log(`${new Date().toLocaleString()}: ${message}`);
+}
+
+// Helper to get better errors if we miss any promise rejection.
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
+
+// Run
+main();
